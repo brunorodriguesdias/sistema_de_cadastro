94e02c85f401f310819e8df51ea16e93668396b0
Bruno <brunorodriguesdias@gmail.com>
2022-10-09 21:07:37 -0300

    começando a criar a interface

---
+++ sistema_de_cadastro.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+
+cor1 = "#feffff" #branco
+
+janela =Tk()
+janela.title('Cadastro de cliente')
+janela.geometry('900x500')
+janela.config(bg=cor1)
+
+janela.mainloop()
